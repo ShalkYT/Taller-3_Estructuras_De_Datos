2d551883ae4eb60f203891d8a6c72cecd80cf073
Andres <shanicdronyt@gmail.com>
2025-10-05 09:45:36 -0500

    Correcion merge

--- arbolAVL/arbolAVL.h
+++ arbolAVL/arbolAVL.h
@@ -18,11 +18,7 @@ class arbolAVL {
         void rotarIzquierda(Nodo<T>* &n);
         void insert(Nodo<T>* &n, T dato);
         void erase(Nodo<T>* &N, T dato);
-<<<<<<< HEAD
         void inorden(Nodo<T>* n, std::list<T>& lista);
-=======
-        void inOrden(Nodo<T>* n, std::list<T>& lista);
->>>>>>> 26acf8b16cd701130dcf9f05f8fecf1c95ce5223
 
     public:
         // Constructor
